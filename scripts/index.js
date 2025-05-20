397630db8617b069c0e3ce2a62dbad88b28b7e08
Sally <salomeytackie555@gmail.com>
2025-05-20 09:31:07 +0000

    adding new sections

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+// Declare variable to store navbar element
+const navElement =  document.getElementById('navbar');
+// Detect scroll event on document
+document.addEventListener('scroll',function() {
+    // console.log(window.scrollY);
+    if (window.scrollY > 70) {
+        // change navbar bg to blue
+ navElement.style.backgroundColor = '#0779E4'
+            }else{
+        // change navbar bg to transparent
+       navElement.style.backgroundColor = 'transparent'
+    }
+});
+
